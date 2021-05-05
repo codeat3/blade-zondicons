26ab2b6db81a1a08317e0bc33a4ec7d3811a2693
Dries Vints <dries@vints.io>
2021-05-05 17:04:33 +0200

    Add config file

--- src/BladeZondiconsServiceProvider.php
+++ src/BladeZondiconsServiceProvider.php
@@ -5,18 +5,25 @@ declare(strict_types=1);
 namespace BladeUI\Zondicons;
 
 use BladeUI\Icons\Factory;
+use Illuminate\Contracts\Container\Container;
 use Illuminate\Support\ServiceProvider;
 
 final class BladeZondiconsServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->callAfterResolving(Factory::class, function (Factory $factory) {
-            $factory->add('zondicons', [
-                'path' => __DIR__.'/../resources/svg',
-                'prefix' => 'zondicon',
-            ]);
+        $this->registerConfig();
+
+        $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
+            $config = $container->make('config')->get('blade-zondicons', []);
+
+            $factory->add('zondicons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
         });
+    }
+
+    private function registerConfig(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/blade-zondicons.php', 'blade-zondicons');
     }
 
     public function boot(): void
@@ -25,6 +32,10 @@ final class BladeZondiconsServiceProvider extends ServiceProvider
             $this->publishes([
                 __DIR__.'/../resources/svg' => public_path('vendor/blade-zondicons'),
             ], 'blade-zondicons');
+
+            $this->publishes([
+                __DIR__.'/../config/blade-zondicons.php' => $this->app->configPath('blade-zondicons.php'),
+            ], 'blade-zondicons-config');
         }
     }
 }
