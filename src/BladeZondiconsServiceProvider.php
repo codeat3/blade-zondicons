655c736309b0b851bc391306e2fd11e8e828059d
driesvints <driesvints@users.noreply.github.com>
2022-03-29 10:15:10 +0000

    Fix styling

--- src/BladeZondiconsServiceProvider.php
+++ src/BladeZondiconsServiceProvider.php
@@ -17,24 +17,24 @@ final class BladeZondiconsServiceProvider extends ServiceProvider
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('blade-zondicons', []);
 
-            $factory->add('zondicons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+            $factory->add('zondicons', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
         });
     }
 
     private function registerConfig(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/blade-zondicons.php', 'blade-zondicons');
+        $this->mergeConfigFrom(__DIR__ . '/../config/blade-zondicons.php', 'blade-zondicons');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-zondicons'),
+                __DIR__ . '/../resources/svg' => public_path('vendor/blade-zondicons'),
             ], 'blade-zondicons');
 
             $this->publishes([
-                __DIR__.'/../config/blade-zondicons.php' => $this->app->configPath('blade-zondicons.php'),
+                __DIR__ . '/../config/blade-zondicons.php' => $this->app->configPath('blade-zondicons.php'),
             ], 'blade-zondicons-config');
         }
     }
